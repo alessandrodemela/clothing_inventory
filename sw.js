19c89f25a5640a6244c46bd7fcb825b7ce7b131f
Alessandro Demela <alessandro.demela@gmail.com>
2026-07-31 22:43:28 +0200

    bumped cache

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
    • Tutto il resto → Cache-first  (shell offline immediata)
    ========================================================================== */
 
-const CACHE = 'guardaroba-v2';
+const CACHE = 'guardaroba-v3';
 
 const PRECACHE = [
   '/index.html',
